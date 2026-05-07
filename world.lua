0562a1a2cf463e7094aac9a58ea90b2a2cd4ac30
Ghosty <dcieszynski99@gmail.com>
2026-05-07 09:52:31 +0200

    Add world table

---
+++ world.lua
@@ -0,0 +1,50 @@
+--world--
+world = {}
+
+function world:new(obj)
+	obj = obj or {}
+	setmetatable(obj, self)
+	self.__index = self
+	obj.entities = {}
+	return obj
+end
+
+function world:update()
+ for e in all(self.entities) do
+  if e.alive then e:update() end
+ 	end
+ 	for e in all(self.entities) do
+  	if e.alive and e.is_enemy then
+  		for b in all(self.entities) do
+   		if b.alive and b.is_bullet and e:overlaps(b) then
+    	 	e.alive = false
+      	b.alive = false
+    	end
+  	end
+  	if e:overlaps(the_player) then
+      	the_player.alive = false
+  	end
+		end
+	end
+ for e in all(self.entities) do
+  if not e.alive then del(self.entities, e) end
+ end
+end
+
+function world:draw()
+	for e in all(self.entities) do
+			if e.alive then e:draw() end
+	end
+end
+
+function world:add_entity(entity)
+	add(self.entities, entity)
+end
+
+function world:check_overlaps(a, group)
+  for b in all(group) do
+    if b.alive and a:overlaps(b) then
+      return b
+    end
+  end
+end
